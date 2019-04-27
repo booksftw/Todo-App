e2c6e78a7b7d41d6e738fe45212b2eab53b348d0
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-04-27 14:16:46 -0700

    save checkpoint

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { random as _random, remove as _remove } from 'lodash'
 import { MatTableDataSource } from '@angular/material';
+import { SelectionModel } from '@angular/cdk/collections';
 
 export interface PeriodicElement {
   name: string;
@@ -22,38 +23,65 @@ let ELEMENT_DATA: any = [
   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
+let LIST_DATA: any = [
+  { position: 1, task: 'a task for the beach' },
+  { position: 2, task: 'a task for the beach' },
+  { position: 3, task: 'a task for the beach' },
+  { position: 4, task: 'a task for the beach' },
+  { position: 5, task: 'a task for the beach' },
+  { position: 6, task: 'a task for the beach' },
+  { position: 7, task: 'a task for the beach' },
+  { position: 8, task: 'a task for the beach' },
+  { position: 9, task: 'a task for the beach' },
+  { position: 10, task: 'a task for the beach' },
+]
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit, AfterViewInit {
-  // Array of list items
+  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<any>(LIST_DATA);
+  selection = new SelectionModel<any>(true, []);
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource() //ELEMENT_DATA;
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  nzClick() {
+    console.log('nz click')
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
 
   removeIndex(index: number, data: any): PeriodicElement[] {
     let newArr;
     newArr = data.filter((e) => e.position !== index)
-    console.log(newArr, index, 'newArr')
-    // const newArr = ELEMENT_DATA.splice(index, 1)
-    // ELEMENT_DATA = newArr
-    // console.log(index, ELEMENT_DATA)
     return newArr
   }
 
   ngAfterViewInit() {
-    setInterval(_ => {
-      this.dataSource.data = this.removeIndex(_random(0, ELEMENT_DATA.length - 1), ELEMENT_DATA)
-    }, 4000)
-    // ELEMENT_DATA.push({ position: 11, name: 'Neon', weight: 20.1797, symbol: 'Ne' })
-    // this.dataSource._update
-    // let i = 0
+    setTimeout(_ => console.log(this.checkboxLabel()), 3000)
     // setInterval(_ => {
-    //   ELEMENT_DATA = this.removeIndex(i)
-    //   console.log(ELEMENT_DATA)
-    // }, 1000)
+    //   this.dataSource.data = this.removeIndex(_random(0, ELEMENT_DATA.length - 1), ELEMENT_DATA)
+    // }, 4000)
   }
 
   ngOnInit() {
