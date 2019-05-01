9368d871a5b16f7d5098de23ce398d48833f81b6
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-05-01 02:22:43 -0700

    cleaned up and save

--- src/app/core/todo.service.ts
+++ src/app/core/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { map, take, toArray } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 export interface TodoTable {
@@ -15,53 +15,33 @@ export interface TodoTask {
   completed: boolean
 }
 
-// `export interface todoTablePayload {
-//   firebaseId: string
-//   // position: number
-//   task: TodoTask
-// }`
-
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  // localTodoTable: CategoryTablePayloadContainer[] = []
-  // Todo Update Type
-  public todoTables: any;
   private todoTablesCollection: AngularFirestoreCollection<any>
+  public todoTables: Observable<TodoTable[]>;
 
   constructor(private db: AngularFirestore) {
     console.log('SERVICE HERE')
     this.todoTablesCollection = this.db.collection('todoTables')
-    // Init local state
+
+    // Shape and Extract Data
     this.todoTables = this.todoTablesCollection.snapshotChanges().pipe(
       map(e => {
         return e.map(el => {
           const payloadData = el.payload.doc.data()
           const position = payloadData.position
           const firebaseId = el.payload.doc.id
-          // this.localTodoTable.push({ firebaseId, ...payloadData })
           return { firebaseId, ...payloadData }
         })
       })
     )
-    // .subscribe(console.log)
   }
+
   getAllTables(): Observable<TodoTable[]> {
     console.log('this todo tables', this.todoTables)
     return this.todoTables
   }
 
-  // Todo remove depercated
-  // getSingleItemCollectionForInit(): Observable<any> {
-  //   const allTodoTablesFiredOnce: Observable<any> = this.todoTablesCollection
-  //     .valueChanges()
-  //     .pipe(toArray())
-  //     .pipe(take(1))
-  //   return allTodoTablesFiredOnce
-  // }
-
-  // getAllLocalTodoTablesPayload(): CategoryTablePayloadContainer[] {
-  //   return this.localTodoTable
-  // }
 }
--- src/app/todo-container/todo-container.component.ts
+++ src/app/todo-container/todo-container.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, ViewChild, ComponentFactoryResolver, Type } from '@angular/core';
-import { toArray, take } from 'rxjs/operators';
 import { TodoService, TodoTask } from '../core/todo.service';
 import { TodoHostDirective } from '../shared/todo-host.directive';
 import { TodoPresentationComponent } from './todo-presentation/todo-presentation.component'
 import { MatTableDataSource } from '@angular/material';
 
 export interface TodoTableData {
-  // Todo Update Interface
   firebaseId: string,
   tableName: string,
   tasks: TodoTask[]
@@ -28,11 +26,8 @@ export class TodoContainerComponent implements OnInit {
 
   ngOnInit() {
     this.initTodoTableComponents()
-
-    // this.todoService.getAllTodosTables()
-    // Make local items in here with the collect
-    // // this.todoService.getLocalItems()
   }
+
   initTodoTableComponents() {
     this.todoService.getAllTables()
       .subscribe(arr => {
@@ -43,6 +38,7 @@ export class TodoContainerComponent implements OnInit {
 
       })
   }
+
   renderTodoComponent(component: Type<TodoPresentationComponent>, data: TodoTableData) {
     console.log('dynamic generate factory test')
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(component)
@@ -53,24 +49,4 @@ export class TodoContainerComponent implements OnInit {
     componentRef.instance.tableName = data.tableName
   }
 
-  databaseStateSetupIntialView() {
-    // The intial database data setup
-    // Get it here and pass it to property in presentation
-    // Pass this observable into the presentation object and it'll subscribe on the inside. Don't forget to unsubscribe.
-    // this.itemsCollection
-    //   .valueChanges()
-    //   .pipe(take(1))
-    //   .pipe(toArray())
-    //* Subscribe in child component
-    // .subscribe((t) => {
-    //   console.log('database update view', t)
-    //   // Intial Table Sorted Data 
-    //   // TODO Update to get from table data object
-    //   this.dataSource.data = _orderby(t[0], ['completed'], ['asc']);
-    //   // Setup Intial Selected State
-    //   this.dataSource.data.map((e, i) => {
-    //     e.completed ? this.selection.select(this.dataSource.data[i]) : null
-    //   })
-    // })
-  }
 }
